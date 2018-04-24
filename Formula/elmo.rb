0ec41c7a88135890b44b42faf58a6ee0533b2b76
Jozef Dransfield <Jozef.Dransfield@me.com>
2018-04-24 22:07:16 +0100

    elmo 0.04 release

--- Formula/elmo.rb
+++ Formula/elmo.rb
@@ -1,9 +1,9 @@
 class Elmo < Formula
   desc "Keep your mac up to date"
   homepage "https://github.com/jozefdransfield"
-  url "https://github.com/jozefdransfield/homebrew-tap/raw/master/Releases/elmo/elmo-0.0.3.tar.gz"
-  sha256 "67f6fcc558ca83b27d8dfbfb0e9ba038c9b618912290e3d9f876d1d3bd65cf32"
-  version "0.0.3"
+  url "https://github.com/jozefdransfield/homebrew-tap/raw/master/Releases/elmo/elmo-0.0.4.tar.gz"
+  sha256 "02f25a4ffcdf02e2624c3440d8ff11a9c2e449644772f103ad98f65911bc4bb0"
+  version "0.0.4"
 
   #depends_on "curl"
 
